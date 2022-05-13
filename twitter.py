f7a4377a1aa9c54bb525588f844094197f93f7fb
Suchandra Jindam <jindam@vizexperts.com>
2022-05-13 18:39:12 +0530

    twitter question changes

--- twitter.py
+++ twitter.py
@@ -1,18 +1,30 @@
-import requests
-from bs4 import BeautifulSoup
+# import requests
+# from bs4 import BeautifulSoup
 
-url = "https://twitter.com/kfc"
+# url = "https://twitter.com/kfc"
 
-pageContent = requests.get(url)
+# pageContent = requests.get(url)
 
-# htmlContent = pageContent.content
+# # htmlContent = pageContent.content
 
-soup = BeautifulSoup(pageContent.content, 'html.parser')
+# soup = BeautifulSoup(pageContent.content, 'html.parser')
 
-# print(soup.prettify())
 
-# title = soup.title
-follow_box = soup.find('div',{'class':'css-1dbjc4n r-1mf7evn'})
-print(follow_box)
-# followers = follow_box.find('a').find('span',{'class':'ProfileNav-value'})
-# print("Number of followers: {} ".format(followers.get('data-count')))
+# # print(soup.prettify())
+
+# # title = soup.title
+# follow_box = soup.find('div',{'class':'css-1dbjc4n'})
+# print(follow_box)
+
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+driver = webdriver.Firefox(executable_path='./geckodriver', options=options)
+driver.implicitly_wait(10)
+
+driver.get('https://twitter.com/kfc')
+
+twt_follwing = driver.find_element_by_css_selector('a[href="/kfc/following"] > span > span').text
+twt_follwers = driver.find_element_by_css_selector('a[href="/kfc/followers"] > span > span').text
+
+print(twt_follwers,twt_follwing)
